b6799c7ec571d3ce641053241c4827633389c833
FredrikAndreasson <fredrik.andreasson@yandex.com>
2021-04-29 17:17:44 +0200

    Base for ability handling

---
+++ Arcade Arena/Abilites/IceblockAbility.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arcade_Arena.Abilites
+{
+    class IceblockAbility : Ability
+    {
+        public IceblockAbility(string userName) : base(userName)
+        {
+
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Arcade Arena/Ability.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arcade_Arena
+{
+    abstract class Ability
+    {
+        protected string userName;
+        protected Vector2 position;
+        protected SpriteAnimation currentAnimation;
+
+        public Ability(string userName)
+        {
+            this.userName = userName;
+        }
+
+        public string UserName => userName;
+
+        public abstract void Update();
+
+        public abstract void Draw(SpriteBatch spriteBatch);
+    }
+}
