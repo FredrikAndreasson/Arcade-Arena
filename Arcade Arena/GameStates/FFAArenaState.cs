749004f2c74dca53c3d28fba5c88505068191ad2
plsegott <baetsailray@hotmail.com>
2021-05-07 10:34:12 +0200

    Lava updated according to code review

--- Arcade Arena/GameStates/FFAArenaState.cs
+++ Arcade Arena/GameStates/FFAArenaState.cs
@@ -36,7 +36,7 @@ namespace Arcade_Arena
             networkManager = new NetworkManager();
 
             player = new Wizard(new Vector2(Window.ClientBounds.Width / 2, Window.ClientBounds.Height / 2), AssetManager.WizardSpriteSheet, 3f, 0.0);
-            lava = new Lava(Game1.graphics.GraphicsDevice, 400);
+            lava = new Lava(Game1.graphics.GraphicsDevice, Window);
             playerManager = new PlayerManager(networkManager, player);
             abilityManager = new AbilityManager(networkManager, playerManager);
 
