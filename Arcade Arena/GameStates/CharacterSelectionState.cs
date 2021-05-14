b596bf7a4749e957b6cf6095f28d57627e389617
plsegott <baetsailray@hotmail.com>
2021-05-14 15:49:59 +0200

    qewfs
    asdf

--- Arcade Arena/GameStates/CharacterSelectionState.cs
+++ Arcade Arena/GameStates/CharacterSelectionState.cs
@@ -60,6 +60,8 @@ namespace Arcade_Arena.GameStates
                 }
                 else if (ogreRect.Contains(MouseKeyboardManager.MousePosition.ToPoint()))
                 {
+                    player = new Ogre(new Vector2(Window.ClientBounds.Width / 2, Window.ClientBounds.Height / 2), 3f, 0.0);
+                    state = States.FFA;
                     
                 }
                 else if (huntressRect.Contains(MouseKeyboardManager.MousePosition.ToPoint()))
