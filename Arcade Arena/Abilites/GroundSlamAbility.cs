b4eb6c4d64bacf4a2558d95b942cdeaad58a9c87
plsegott <baetsailray@hotmail.com>
2021-05-17 17:31:54 +0200

    slam final

--- Arcade Arena/Abilites/GroundSlamAbility.cs
+++ Arcade Arena/Abilites/GroundSlamAbility.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Arcade_Arena.Classes;
+using Arcade_Arena.Library;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,36 @@ using System.Threading.Tasks;
 
 namespace Arcade_Arena.Abilites
 {
-    class GroundSlamAbility
+    
+
+    class GroundSlamAbility : Ability
     {
+        Ogre player;
+
+        public GroundSlamAbility(Ogre player)
+        {
+            this.player = player;
+
+            Type = AbilityOutline.AbilityType.AbilityOne;
+            currentAnimation= new SpriteAnimation(AssetManager.groundSmashCrackle, new Vector2(0, 0), new Vector2(4, 0), new Vector2(71, 71), new Vector2(4, 0), 500);
+
+            this.position = player.Position-new Vector2(71, 71);
+
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+          //  currentAnimation.Draw(spriteBatch, position, 0.0f, Vector2.Zero, Game1.SCALE);
+        }
+
+        public override void Update()
+        {
+            currentAnimation.Update();
+            if (currentAnimation.Loop > 0 && currentAnimation.XIndex == 0)
+            {
+                isDead = true;
+            }
+        }
+
     }
 }
