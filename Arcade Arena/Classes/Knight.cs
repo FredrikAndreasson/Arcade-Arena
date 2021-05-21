a862c4095fc1610b1b6b5e41df397549a03bb3ef
plsegott <baetsailray@hotmail.com>
2021-05-21 14:07:15 +0200

    knight v1

--- Arcade Arena/Classes/Knight.cs
+++ Arcade Arena/Classes/Knight.cs
@@ -11,6 +11,7 @@ namespace Arcade_Arena.Classes
 {
     class Knight : Character
     {
+        SpriteAnimation idleAnimation;
         SpriteAnimation walkingAnimation;
         SpriteAnimation backwardsAnimation;
         SpriteAnimation groundSmashOgreAnimation;
@@ -31,6 +32,9 @@ namespace Arcade_Arena.Classes
 
         public Knight(Vector2 position, float speed, double direction) : base(position, speed, direction)
         {
+
+            idleAnimation = new SpriteAnimation(AssetManager.KnightSpriteSheet, new Vector2(1, 2), new Vector2(2, 2), frameSize, sheetSize, 1000);
+
             walkingAnimation = new SpriteAnimation(AssetManager.KnightSpriteSheet, new Vector2(0, 0), new Vector2(6, 0), frameSize, sheetSize, 300);
             backwardsAnimation = new SpriteAnimation(AssetManager.KnightSpriteSheet, new Vector2(0, 3), new Vector2(2, 3), frameSize, sheetSize, 150);
             groundSmashOgreAnimation = new SpriteAnimation(AssetManager.KnightSpriteSheet, new Vector2(0, 2), new Vector2(3, 2), frameSize, sheetSize, 125);
@@ -41,14 +45,56 @@ namespace Arcade_Arena.Classes
 
             shadow = new Shadow(position, AssetManager.WizardShadow, speed, direction);
 
+            ChangeAnimation(ref currentAnimation, idleAnimation);
+
             speed = 1;
         }
         public override void Update()
         {
             currentAnimation.Update();
             UpdateCooldowns();
-            //kanske ändra till "actionable" debuffs sen istället för att kolla om man är i varje ability
-            if (!inGroundSmash && !inBodySlam)
+            CheckAbilityUse();
+
+            if (inGroundSmash)
+            {
+
+                groundSmashAnimation.Update();
+
+                if (groundSmashAnimation.XIndex >= 4)
+                {
+                    inGroundSmash = false;
+                    middleOfSprite = new Vector2(Position.X + 35, Position.Y + 60);
+                    aimDirection = UpdateAimDirection();
+                    CheckRegularAnimation();
+                }
+            }
+            else if (inBodySlam)
+            {
+                bodySlamAnimation.Update();
+                if ((bodySlamCooldown <= 2f))
+                {
+                    inBodySlam = false;
+                    middleOfSprite = new Vector2(Position.X + 35, Position.Y + 60);
+                    aimDirection = UpdateAimDirection();
+                    CheckRegularAnimation();
+                }
+            }
+            else
+            {
+                if (!Stunned)
+                {
+                    CheckRegularAnimation();
+                }
+                middleOfSprite = new Vector2(Position.X + 35, Position.Y + 60);
+                base.Update();
+            }
+
+            shadow.Update(Position);
+        }
+
+        private void CheckAbilityUse()
+        {
+            if (!inGroundSmash && !inBodySlam && !Stunned)
             {
                 if (Keyboard.GetState().IsKeyDown(Keys.E) && groundSmashCooldown <= 0)
                 {
@@ -61,36 +107,25 @@ namespace Arcade_Arena.Classes
                     bodySlamCooldown = 5;
                 }
             }
+        }
 
-            if (inGroundSmash)
+        private void CheckRegularAnimation()
+        {
+            if (walking)
             {
-
-                groundSmashAnimation.Update();
-
-                if (groundSmashAnimation.XIndex >= 4)
+                if (WalkingBackwards())
                 {
-                    inGroundSmash = false;
-                    middleOfSprite = new Vector2(Position.X + 35, Position.Y + 60);
-                    aimDirection = UpdateAimDirection();
+                    ChangeAnimation(ref currentAnimation, backwardsAnimation);
                 }
-            }
-            else if (inBodySlam)
-            {
-                bodySlamAnimation.Update();
-                if ((bodySlamCooldown <= 2f))
+                else
                 {
-                    inBodySlam = false;
-                    middleOfSprite = new Vector2(Position.X + 35, Position.Y + 60);
-                    aimDirection = UpdateAimDirection();
+                    ChangeAnimation(ref currentAnimation, walkingAnimation);
                 }
             }
             else
             {
-                middleOfSprite = new Vector2(Position.X + 35, Position.Y + 60);
+                ChangeAnimation(ref currentAnimation, idleAnimation);
             }
-                base.Update();
-
-            shadow.Update(Position);
         }
 
         private void UpdateCooldowns()
@@ -101,23 +136,30 @@ namespace Arcade_Arena.Classes
 
         public override void Draw(SpriteBatch spriteBatch)
         {
+            if (isDead)
+            {
+                DrawAnimations(spriteBatch);
+                return;
+            }
+
             shadow.Draw(spriteBatch);
+            currentAnimation.Draw(spriteBatch, lastPosition, 0.0f, Vector2.Zero, Game1.SCALE);
+            base.Draw(spriteBatch);
+        }
 
-
+        private void DrawAnimations(SpriteBatch spriteBatch)
+        {
             if (inGroundSmash)
             {
-                // groundSmashAnimation.Draw(spriteBatch, Position - new Vector2(71, 71), 0.0f, Vector2.Zero, Game1.SCALE);
+              //  teleportOutAnimation.Draw(spriteBatch, newPosition, 0.0f, Vector2.Zero, Game1.SCALE);
+               // handTeleportOutAnimation.Draw(spriteBatch, newPosition, 0.0f, Vector2.Zero, Game1.SCALE);
             }
             else if (inBodySlam)
             {
-                // bodySlamAnimation.Draw(spriteBatch, position, 0.0f, Vector2.Zero, Game1.SCALE);
+               // iceBlockAnimation.Draw(spriteBatch, Position, 0.0f, Vector2.Zero, Game1.SCALE);
             }
-            else
-            {
-                currentAnimation = walkingAnimation;
-            }
-            currentAnimation.Draw(spriteBatch, Position, 0.0f, Vector2.Zero, Game1.SCALE);
         }
+
 
         private void GroundSmash()
         {
