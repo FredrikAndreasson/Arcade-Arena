bb410ca7f8f740b2eb03e2da802f15b406dde8b0
FredrikAndreasson <fredrik.andreasson@yandex.com>
2021-05-27 16:44:30 +0200

    Update Knight.cs

--- Arcade Arena/Classes/Knight.cs
+++ Arcade Arena/Classes/Knight.cs
@@ -147,7 +147,7 @@ namespace Arcade_Arena.Classes
 
             shadow.Draw(spriteBatch);
             DrawAnimations(spriteBatch);
-            currentAnimation.Draw(spriteBatch, lastPosition, 0.0f, Vector2.Zero, Game1.SCALE);
+            currentAnimation.Draw(spriteBatch, LastPosition, 0.0f, Vector2.Zero, Game1.SCALE);
             base.Draw(spriteBatch);
         }
 
